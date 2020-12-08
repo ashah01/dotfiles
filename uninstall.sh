688a8d0f641930c4973fa66d2694200fc30cecf9
Arnav Shah <arnavshah@MacBook-Pro.local>
2020-12-07 22:18:33 -0500

    Added uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+rm -rf $HOME/.vimrc
+rm -rf $HOME/.vim
+rm -rf $HOME/.oh-my-zsh
+rm -rf $HOME/.zsh_history
+rm -rf $HOME/.zcompdump-MacBook\ Pro-5.8 
+rm -rf $HOME/.zshrc
+rm -rf ./Brewfile
+touch Brewfile
+brew bundle --force cleanup
+rm -rf .
