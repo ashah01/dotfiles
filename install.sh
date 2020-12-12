fc9ed8099dd040d8dd0964b8fd65b74e4dd8f8e6
ashah01 <40141129+ashah01@users.noreply.github.com>
2020-12-12 17:46:32 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,6 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 vim -E -s -u ~/.vimrc +PlugInstall +qall
 
 # ZSH configuration
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$HOME/.oh-my-zsh/custom/themes/spaceship-prompt" --depth=1
 git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
