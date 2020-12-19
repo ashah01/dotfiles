556aefd6f573562d799afaeff4a7df767e9edd7c
ashah01 <40141129+ashah01@users.noreply.github.com>
2020-12-19 17:26:31 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,3 @@
-# ZSH configuration
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$HOME/.oh-my-zsh/custom/themes/spaceship-prompt" --depth=1
-git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
-mv .zshrc ~
-
-source ~/.zshrc
-
 # Install tools + apps
 brew bundle
 
