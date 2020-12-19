f1c5255bfd63153e455c7b67dab55ee5c6bba430
ashah01 <40141129+ashah01@users.noreply.github.com>
2020-12-19 17:27:42 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,5 @@
-rm -rf $HOME/.oh-my-zsh
-rm -rf $HOME/.zsh_history
-rm -rf $HOME/.zcompdump-MacBook\ Pro-5.8 
-rm -rf $HOME/.zshrc
 rm -rf ./Brewfile
 touch Brewfile
 brew bundle --force cleanup
-rm -rf ../dotfiles
+cd ..
+rm -rf dotfiles/
