371fc45ee33cbde1d1ba8ab60e3c95e44f70236a
Arnav Shah <40141129+ashah01@users.noreply.github.com>
2025-02-25 20:10:19 -0500

    Update colorscheme.lua

--- nvim/lua/arnavshah/lazy/colorscheme.lua
+++ nvim/lua/arnavshah/lazy/colorscheme.lua
@@ -1,7 +1,16 @@
 return {
-  "nyoom-engineering/oxocarbon.nvim"
-  -- Add in any other configuration; 
-  --   event = foo, 
-  --   config = bar
-  --   end,
+    "rebelot/kanagawa.nvim",
+    config = function()
+        require("kanagawa").setup({
+            theme = "dragon",
+            background = {   -- map the value of 'background' option to a theme
+                dark = "dragon", -- try "dragon" !
+                light = "lotus"
+            },
+        })
+    end
+    -- Add in any other configuration;
+    --   event = foo,
+    --   config = bar
+    --   end,
 }
