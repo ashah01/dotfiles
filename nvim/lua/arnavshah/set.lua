6f6fce900f0bc798d909e20c31ba607dd183b8f8
Arnav Shah <40141129+ashah01@users.noreply.github.com>
2025-03-19 14:22:22 -0400

    Update set.lua

--- nvim/lua/arnavshah/set.lua
+++ nvim/lua/arnavshah/set.lua
@@ -1,5 +1,5 @@
 vim.opt.background = "dark" -- set this to dark or light
-vim.cmd.colorscheme("kanagawa")
+vim.cmd.colorscheme "gruvbox"
 
 vim.opt.clipboard = 'unnamedplus' -- use system keyboard for yank
 
